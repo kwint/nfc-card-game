95b4d52fd214001e4b7f02c5aae957c2bb30502a
Quint van Dijk <quintvdijk@gmail.com>
2024-04-01 22:43:14 +0200

    Added links to admin list

--- nfc_card_game/main/admin.py
+++ nfc_card_game/main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from nfc_card_game.main.models import Mine, Player, Post, Team, TeamMine
 
@@ -12,6 +15,13 @@ class PlayerInLine(admin.TabularInline):
 class TeamMineInLine(admin.TabularInline):
     model = TeamMine
 
+class TeamMineAdmin(admin.ModelAdmin):
+    list_display = ["mine", "team", "link"]
+
+    @mark_safe
+    def link(self, obj):
+        card_url = reverse("mine", kwargs={"card_uuid": obj.card_uuid})
+        return format_html(f'<a href="{card_url}">link</a>')
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ["name"]
@@ -19,11 +29,22 @@ class TeamAdmin(admin.ModelAdmin):
 
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ["name", "card_uuid", "team", "section"]
+    list_display = ["name", "card_uuid", "team", "section", "link"]
+
+    @mark_safe
+    def link(self, obj):
+        card_url = reverse("player", kwargs={"card_uuid": obj.card_uuid})
+        return format_html(f'<a href="{card_url}">link</a>')
+
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ["name", "card_uuid", "buys", "sells"]
+
+    @mark_safe
+    def link(self, obj):
+        card_url = reverse("post", kwargs={"card_uuid": obj.card_uuid})
+        return format_html(f'<a href="{card_url}">link</a>')
 
 
 class MineAdmin(admin.ModelAdmin):
@@ -35,4 +56,4 @@ admin.site.register(Team, TeamAdmin)
 admin.site.register(Player, PlayerAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Mine, MineAdmin)
-admin.site.register(TeamMine)
+admin.site.register(TeamMine, TeamMineAdmin)
