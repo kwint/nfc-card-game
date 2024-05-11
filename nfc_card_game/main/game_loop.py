c7675809075a712591bb94ea8b19bdf04c70f6c3
timvisee <tim@visee.me>
2024-05-11 19:11:42 +0200

    In game loop, broadcast each iteration over API channel

--- nfc_card_game/main/game_loop.py
+++ nfc_card_game/main/game_loop.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from schedule import Scheduler
+from asgiref.sync import async_to_sync
+import channels
 import threading
 import time
 from pydantic import BaseModel, Field
 from nfc_card_game.main.models.trading import MinerType, TeamMine, TeamMineItem
 import logging
+
+from . import api_consumer
 
 logger = logging.getLogger(__name__)
 
@@ -36,6 +40,18 @@ def update_team_mines():
     team_mines = TeamMine.objects.all()
     for team_mine in team_mines:
         update_team_mine(team_mine)
+
+    # Bump all API clients, update their mine state
+    # TODO: we want to do this in a more efficient way in the future
+    channel_layer = channels.layers.get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        api_consumer.CHANNEL_NAME,
+        {
+            "type": "event_handler",
+            "event_id": api_consumer.ChannelEventType.REFRESH_MINE_STATE.value,
+            "data": {},
+        }
+    )
 
 
 def update_team_mine(team_mine: TeamMine):
