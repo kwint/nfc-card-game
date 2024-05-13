d147cadaabf6a2d6e835af21ff145f3ccb435e6f
Quint van Dijk <quintvdijk@gmail.com>
2024-05-13 21:27:50 +0200

    Added miner count to api

--- nfc_card_game/main/game_loop.py
+++ nfc_card_game/main/game_loop.py
@@ -69,6 +69,13 @@ def update_team_mine(team_mine: TeamMine):
 
     # Broadcast for API clients: mine money update
     channel_layer = channels.layers.get_channel_layer()
+    miners = {
+        mine_item.item: {
+            "effective": SETTINGS.miner_factors[mine_item.item.name] * mine_item.amount,
+            "actual": mine_item.amount,
+        }
+        for mine_item in mine_items
+    }
     async_to_sync(channel_layer.group_send)(
         api_consumer.CHANNEL_NAME,
         {
@@ -78,6 +85,7 @@ def update_team_mine(team_mine: TeamMine):
                 "mine_id": team_mine.mine_id,
                 "team_id": team_mine.team_id,
                 "money": team_mine.money,
+                "miners": miners,
             },
         },
     )
