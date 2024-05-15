e4821df36697bf309961bf15c3fca5eea6ca55ca
timvisee <tim@visee.me>
2024-05-15 17:29:04 +0200

    Add button to toggle fullscreen mode

--- dashboard/Scripts/WindowManager.gd
+++ dashboard/Scripts/WindowManager.gd
@@ -5,7 +5,9 @@ func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		self.quit();
 		return;
-
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		self.toggle_fullscreen();
+		return;
 
 func _notification(what):
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
@@ -14,3 +16,10 @@ func _notification(what):
 		
 func quit():
 	self.get_tree().quit();
+
+
+func toggle_fullscreen():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_MAXIMIZED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED);
