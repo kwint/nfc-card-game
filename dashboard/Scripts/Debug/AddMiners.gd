5571ac8bdae8621da5d8414ea55b58f65cf153f1
timvisee <tim@visee.me>
2024-05-15 13:39:57 +0200

    Add debug menu to both sides

--- dashboard/Scripts/Debug/AddMiners.gd
+++ dashboard/Scripts/Debug/AddMiners.gd
@@ -1,6 +1,7 @@
 extends GridContainer
 
 @export var game_controller: Node;
+@export var team: Global.TeamId = Global.TeamId.TEAM1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,14 +12,14 @@ func _ready():
 
 func add1():
 	var amount = $SpinBox1.value;
-	game_controller.add_miners(Global.TeamId.TEAM1, Global.MinerType.MINER1, amount, amount, "Mijnwerker");
+	game_controller.add_miners(self.team, Global.MinerType.MINER1, amount, amount, "Mijnwerker");
 
 
 func add2():
 	var amount = $SpinBox2.value;
-	game_controller.add_miners(Global.TeamId.TEAM1, Global.MinerType.MINER2, amount, 3 * amount, "Drilboor");
+	game_controller.add_miners(self.team, Global.MinerType.MINER2, amount, 3 * amount, "Drilboor");
 
 
 func add3():
 	var amount = $SpinBox3.value;
-	game_controller.add_miners(Global.TeamId.TEAM1, Global.MinerType.MINER3, amount, 10 * amount, "Bulldozer");
+	game_controller.add_miners(self.team, Global.MinerType.MINER3, amount, 10 * amount, "Bulldozer");
