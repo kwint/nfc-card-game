efd3769f4b4f3e086ebe5aee3129b0aac706cafb
Quint van Dijk <quintvdijk@gmail.com>
2024-05-15 21:00:30 +0200

    comments

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -4,9 +4,7 @@
 python manage.py collectstatic --noinput
 
 if [ "$1" = "--debug" ]; then
-  # Django development server
   exec python manage.py runserver 0.0.0.0:8000
 else
-  # Gunicorn
   exec daphne -b 0.0.0.0 -p 8000 nfc_card_game.asgi:application
 fi
